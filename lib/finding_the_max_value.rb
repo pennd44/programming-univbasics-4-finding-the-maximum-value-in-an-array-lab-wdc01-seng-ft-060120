b01b23625a15e3057711bacb4dfaccfeb5c4ee55
Penn M Diba <pdiba@oxbowschool.org>
2020-05-23 07:24:33 +0000

    Automatically backed up by Learn

--- lib/finding_the_max_value.rb
+++ lib/finding_the_max_value.rb
@@ -1,7 +1,9 @@
 def find_max_value(array)
   # Add your solution here
   count=0 
-  while count < array.length 
-  
-end
+  while count < array.length do
+    array[count]
+    
+    count+=1 
+  end
 end
